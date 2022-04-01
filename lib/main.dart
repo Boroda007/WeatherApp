0fce24216a6be551ace5fd98009d851ccc3016b4
Oleg Maksakov <maksakov.ol@yandex.ru>
2022-04-01 21:59:43 +0300

    Init project

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/ui/screens/weather.dart';
+
+void main() {
+  runApp(const Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const WeatherScreen(),
+    );
+  }
+}
